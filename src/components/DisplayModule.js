667d297ef5e9018a380b840832c1ef52597cf7aa
Aman Sehgal <amansehgal0800@gmail.com>
2023-04-09 11:21:51 +0530

    Changes done

--- src/components/DisplayModule.js
+++ src/components/DisplayModule.js
@@ -6,11 +6,33 @@ import Typography from '@mui/material/Typography';
 
 function DisplayModule({module, onDragStart}) {
   return (
-    <Box className="module" onDragStart={(event) => onDragStart ? onDragStart(event, module) : ""} draggable>
+    <Box 
+    className="module" 
+    onDragStart={(event) => onDragStart ? onDragStart(event, module) : ""} 
+    draggable
+    >
         <Grid container spacing={2}>
-            <Grid item xs={2}><Box padding={1} borderRight={"1px solid blue"}><Typography>{module.input_type}</Typography></Box></Grid>
-            <Grid item xs={8}><Box padding={1} borderRight={"1px solid blue"}><Typography>{module.name}</Typography></Box></Grid>
-            <Grid item xs={2}><Box padding={1}><Typography>{module.output_type}</Typography></Box></Grid>
+            <Grid item xs={2}>
+                <Box padding={1} borderRight={"1px solid blue"}>
+                    <Typography>
+                        {module.input_type}
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={8}>
+                <Box padding={1} borderRight={"1px solid blue"}>
+                    <Typography>
+                        {module.name}
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={2}>
+                <Box padding={1}>
+                    <Typography>
+                        {module.output_type}
+                    </Typography>
+                </Box>
+            </Grid>
         </Grid>
      </Box>
   )
